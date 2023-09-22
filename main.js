c876411e2f16d58d81c9325b0a3bd7fe155df99d
LukeBeaumont <lbeaumont50@msn.com>
2023-09-22 20:45:52 +0100

    Finish first module

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
-const playerOneName = document.querySelector("#playerOneName");
-const playerTwoName = document.querySelector("#playerTwoName");
-const startBtn = document.querySelector("#start-btn");
+const initPlayers = (() => {
+  const playerOneName = document.querySelector("#playerOneName");
+  const playerTwoName = document.querySelector("#playerTwoName");
+  const startBtn = document.querySelector("#start-btn");
 
-const createPlayer = (name) => {
-  return { name };
-};
+  function addEventListener() {
+    startBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      getPlayers();
+      gamePlay.setTurn();
+    });
+  }
+  const createPlayer = (name) => {
+    return { name };
+  };
+  function getPlayers() {
+    const playerOne = createPlayer(playerOneName.value);
+    playerOne.mark = "x";
+    const playerTwo = createPlayer(playerTwoName.value);
+    playerTwo.mark = "o";
+    console.log(playerOne, playerTwo);
+  }
 
-startBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  getPlayers();
-});
+  return { addEventListener, getPlayers };
+})();
 
-function getPlayers() {
-  const playerOne = createPlayer(playerOneName.value);
-  playerOne.mark = "x";
-  const playerTwo = createPlayer(playerTwoName.value);
-  playerTwo.mark = "o";
-  console.log(playerOne, playerTwo);
-}
+initPlayers.addEventListener();
+
+const gamePlay = (() => {
+  const gameBoard = document.querySelector(".game-board");
+  let circleTurn = false;
+
+  function setTurn() {
+    if (circleTurn) {
+      gameBoard.classList.remove("x");
+      gameBoard.classList.add("o");
+    } else {
+      gameBoard.classList.remove("o");
+      gameBoard.classList.add("x");
+    }
+  }
+  return { setTurn };
+})();
